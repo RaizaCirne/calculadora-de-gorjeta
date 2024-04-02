5d8a3adcc8640a9f997c284d479d95b45e8df9d6
Raiza Cirne <raizacirne@gmail.com>
2024-04-02 12:09:24 -0300

    Feat: add html via js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,9 @@ function atualizar() {
   let total = conta + valorGorjeta;
   let valorDivididoPorPessoas = total / pessoas;
 
- 
+  document.getElementById('porcentagemGorjeta').innerHTML = porcentagemGorjeta + '%';
+  document.getElementById('valorGorjeta').innerHTML = 'R$ ' + valorGorjeta;
+  document.getElementById('totalComGorjeta').innerHTML = 'R$ ' + total;
+  document.getElementById('pessoas').innerHTML = pessoas + ' Pessoas';
+  document.getElementById('divisaoConta').innerHTML = valorDivididoPorPessoas;
 };
